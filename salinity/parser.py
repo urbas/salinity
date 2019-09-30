efb200aca8e1d38ed062c06d91bd277d8cb3281d
Matej Urbas <matej.urbas@gmail.com>
2019-09-30 08:48:57 +0100

    Implements multiline parsing support

--- salinity/parser.py
+++ salinity/parser.py
@@ -1,11 +1,38 @@
+from collections import deque
+
+
 def parse_changes_fmt(text):
     """
     Takes Salt's state output in the `--state-output=changes` format and returns a dictionary.
     """
+    raw_lines = deque(text.splitlines())
     parsed_entries = []
-    for line in text.splitlines():
-        parsed_entries.append(parse_clean_entry(line))
+    current_entry = None
+    current_entry_is_clean = True
+
+    while raw_lines:
+        line = raw_lines.popleft()
+        starts_with_name = line.startswith("  Name: ")
+        starts_with_dashes = line.startswith("----------")
+        if starts_with_name or starts_with_dashes:
+            if current_entry is not None:
+                parsed_entries.append(
+                    parse_entry(current_entry, current_entry_is_clean)
+                )
+            current_entry_is_clean = starts_with_name
+            current_entry = line
+        else:
+            current_entry += "\n" + line
+    if current_entry is not None:
+        parsed_entries.append(parse_entry(current_entry, current_entry_is_clean))
+
     return parsed_entries
+
+
+def parse_entry(entry, is_clean):
+    if is_clean:
+        return parse_clean_entry(entry)
+    return parse_changed_entry(entry)
 
 
 def parse_clean_entry(clean_entry):
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,6 +1,85 @@
 from salinity import parser
 
 
+def test_multiple_clean_entries():
+    assert [
+        {
+            "name": "/etc/salt/grains",
+            "function": "file.managed",
+            "result": "Clean",
+            "duration_ms": 65.815,
+        },
+        {
+            "name": "localhost",
+            "function": "host.present",
+            "result": "Clean",
+            "duration_ms": 1.721,
+        },
+    ] == parser.parse_changes_fmt(
+        """  Name: /etc/salt/grains - Function: file.managed - Result: Clean Started: - 10:16:58.807965 Duration: 65.815 ms
+  Name: localhost - Function: host.present - Result: Clean Started: - 10:16:59.791990 Duration: 1.721 ms"""
+    )
+
+
+def test_multiple_mixed_entries():
+    assert (
+        [
+            {
+                "name": "/etc/salt/grains",
+                "function": "file.managed",
+                "result": "Clean",
+                "duration_ms": 65.815,
+            },
+            {
+                "name": "/etc/salt/grains",
+                "function": "file.managed",
+                "result": "True",
+                "duration_ms": 51.236,
+            },
+            {
+                "name": """[ -f /swapfile ] || fallocate -l 4096M /swapfile
+chmod 0600 /swapfile
+mkswap /swapfile
+swapon -a""",
+                "function": "cmd.run",
+                "result": "Clean",
+                "duration_ms": 480.786,
+            },
+            {
+                "name": "localhost",
+                "function": "host.present",
+                "result": "Clean",
+                "duration_ms": 1.721,
+            },
+        ]
+        == parser.parse_changes_fmt(
+            """  Name: /etc/salt/grains - Function: file.managed - Result: Clean Started: - 10:16:58.807965 Duration: 65.815 ms
+----------
+          ID: /etc/salt/grains
+    Function: file.managed
+      Result: True
+     Comment: File /etc/salt/grains updated
+     Started: 10:16:17.259542
+    Duration: 51.236 ms
+     Changes:
+              ----------
+              diff:
+                  ---
+                  +++
+                  @@ -1,3 +1 @@
+                   roles: []
+                  -foo
+                  -bar
+  Name: [ -f /swapfile ] || fallocate -l 4096M /swapfile
+chmod 0600 /swapfile
+mkswap /swapfile
+swapon -a
+ - Function: cmd.run - Result: Clean Started: - 10:17:20.639763 Duration: 480.786 ms
+  Name: localhost - Function: host.present - Result: Clean Started: - 10:16:59.791990 Duration: 1.721 ms"""
+        )
+    )
+
+
 def test_single_clean_entry():
     assert {
         "name": "/etc/salt/grains",
